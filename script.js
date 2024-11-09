27d94b243785dfef5bf1fa145e27e2c6dd4219cf
Juan <nabuc@LaPropia.lapropia.com>
2024-11-09 13:17:02 -0500

    Version 1.2
    Arreglo de menu desplegable

--- script.js
+++ script.js
@@ -17,4 +17,28 @@ window.addEventListener('resize', () => {
     menuSide.classList.remove('menu__side_move');
   }
 });
-
+//flecha para mostrar menu 
+const botones = document.querySelectorAll('.button');
+botones.forEach(boton => {
+  boton.addEventListener('click', () => {
+    //obtener id de la caja 
+    const targetId = boton.getAttribute('data-target');
+    //selecionar caja correspondiente
+    const caja = document.getElementById(targetId);
+    // eliminar clase opcity de la clase de todos los elementos de la caja
+    if (caja){
+      const elementosOpacity = caja.querySelectorAll('.grid-item-slider');
+      // Alternar visibilidad de los elementos
+      elementosOpacity.forEach(elemento => {
+        //verificar si tiene clase opacity para cambiarla 
+        if (elemento.classList.contains('opacity')){
+          elemento.classList.remove('opacity');
+        } else {
+          elemento.classList.add('opacity')
+        }
+      });
+      // alterna el estado "activado" del boton
+      boton.classList.toggle('activado')
+    }
+  });
+});
